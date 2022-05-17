2312506bb82b374427e4795da74113afab345cdf
Evgunt <Evgunt@mail.ru>
2022-05-17 10:45:32 +0700

    first commit

---
+++ src/Catalog.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Catalog;
+
+use Illuminate\Database\Capsule\Manager as DB;
+
+class Catalog
+{
+    public function getCat()
+    {
+        return DB::select('SELECT * FROM catalog');
+    }
+    public function getItems(int $parent)
+    {
+        return DB::select('SELECT * FROM item WHERE catalog =?', [$parent]);
+    }
+    public function search(string $key)
+    {
+        return DB::select("SELECT * FROM item WHERE title LIKE '%".$key."%'");
+    }
+    public function getItemAll(int $id)
+    {
+        return DB::select("SELECT * FROM item WHERE id=?", [$id]);
+    }
+}
